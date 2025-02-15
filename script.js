db12b91c5aff2235161af570a342e6f1642915bf
erikriva12 <47138914+erikriva12@users.noreply.github.com>
2025-02-15 13:49:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Kmu yakin?",
+    "yakin beneran??",
+    "gamau pikir dulu?",
+    "masa siih...",
+    "Pikir dulu lagi deeh",
+    "Klo milih tidak aku sedih lhoo...",
+    "Sedih banget...",
+    "Sedih bangeeeeeeeet...",
+    "yaudh deh klo gitu...",
+    "Pilih iya dong. Pleaseee.... ❤️"
 ];
 
 let messageIndex = 0;
